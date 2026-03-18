10b5389e823f4f55e3482ae420618029a9c98c78
Jayden Cummins <jaydencummins4@gmail.com>
2026-03-17 23:04:27 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+function updateDisplay(info) {
+    // This finds the div with the ID 'plan-next-route'
+    const display = document.getElementById('plan-next-route');
+    display.textContent = info;
+}
